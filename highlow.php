7c9dfa5c0cba9568645109d1c998d954b31b7496
Felice Malaszowski <felice.vargas@gmail.com>
2014-11-24 11:21:31 -0600

    This adds the code to count the number of guesses.

--- highlow.php
+++ highlow.php
@@ -1,30 +1,29 @@
 <?php
 
 $rand_num = mt_rand(1,100);
-
 // fwrite(STDOUT, 'Can you guess the number?  ');
-	// $userGuess = fgets(STDIN);
-
+$userGuess = fgets(STDIN);
+$count = 0;
 while ($rand_num != $userGuess){	
-	
+	$count++;
 	fwrite(STDOUT, 'Can you guess the number?  ');
 	$userGuess = fgets(STDIN);
 
 	if ($userGuess < $rand_num) {
-		fwrite(STDOUT, "Guess higher :\(\n");
+		fwrite(STDOUT, "Guess higher :(\n");
 		// $userGuess = fgets(STDIN);
 	} 
 
 	elseif ($userGuess > $rand_num) {
-		fwrite(STDOUT, "Guess lower :\(\n");
+		fwrite(STDOUT, "Guess lower :(\n");
 		// $userGuess = fgets(STDIN);
 	}
 		
 	else {
-		fwrite(STDOUT, "You guessed it\! :\)\n");
+		fwrite(STDOUT, "You guessed it! :)\n");
+		echo "$rand_num\n";
+		echo "'You took this many guesses' = $count\n";
 		exit(0);	
 	}
 
 }
-
-// fwrite(STDOUT, "You guessed it\! :\)\n");
