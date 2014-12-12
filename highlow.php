434da52a0d751efb5763cb58200fe3ae7c616eb7
Felice Malaszowski <felice.vargas@gmail.com>
2014-12-12 15:39:53 -0600

    Updated argc$

--- highlow.php
+++ highlow.php
@@ -7,12 +7,27 @@
 	// This only starts after hitting enter twice
 	//echo "arg1 is {$argv[1]} and arg2 is {$argv[2]}\n";
 	// }
-$rand_num = rand($argv[1], $argv[2]);
+// $rand_num = rand($argv[1], $argv[2]);
 // $rand_num = mt_rand(1,100);
-// fwrite(STDOUT, 'Can you guess the number?  ');
+// 
+fwrite(STDOUT, 'Start guessing by hitting enter');
+$userGuess = '';
+
 $userGuess = fgets(STDIN);
+if (is_numeric($argc[1])) {
+	$low=$argc[1];
+} else {
+	$low=1;
+}
+if (is_numeric($argc[2])) {
+	$high=$argc[2];
+} else {
+	$high=9;
+}
+$rand_num = rand($low, $high);
 $count = 0;
-while ($rand_num != $userGuess){	
+
+while($rand_num != $userGuess){	
 	$count++;
 	fwrite(STDOUT, 'Can you guess the number?  ');
 	$userGuess = fgets(STDIN);
